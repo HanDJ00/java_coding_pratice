57d3598bb1b4dc08f4da05f1f4cd906c458d1ec8
HanDJ00 <han06225@gmail.com>
2024-08-13 19:26:39 +0900

    2024-08-13 스터디 (beakjoon 1문제)

--- src/main/java/baekjoon/gold/_1/_1016.java
+++ src/main/java/baekjoon/gold/_1/_1016.java
@@ -1,35 +1,24 @@
-//package baekjoon.gold._1;
-//
-//import java.util.Scanner;
-//
-//public class _1016 {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        long min = sc.nextLong();
-//        long max = sc.nextLong();
-//        int token = 0;
-//        int count = 0;
-//
-//        if (min == 1) min = 2;
-//
-//        for(long i = min; i <= max; i++) {
-//            for(long j = 2; j <= (long)Math.sqrt((double)(i)); j++) {
-//                if(i % (j * j ) == 0) {
-//                    token = 1;
-//                    break;
-//                }
-//            }
-//            if (min == 2 || min == 3) {
-//                count++;
-//            } else if(token == 1) {
-//                token = 0;
-//            } else {
-//                count++;
-//                System.out.println(i);
-//            }
-//
-//        }
-//
-//        System.out.println(count);
-//    }
-//}
+package baekjoon.gold._1;
+
+import java.util.Scanner;
+
+public class _1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long min = sc.nextLong();
+        long max = sc.nextLong();
+        int count = 0;
+
+        for (long i = min; i <= max; i++) {
+
+            for (long j = 2; j * j <= i; j++) {
+                if ((i % (j * j) == 0)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        System.out.println(max - min - count + 1);
+    }
+}
+
